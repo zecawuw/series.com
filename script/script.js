9c808255c589b4ac651e5369961b576568409b58
zecawuw <88804350+zecawuw@users.noreply.github.com>
2022-05-14 22:46:23 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,11 @@
+function mobileMenu(){
+    document.getElementById("m-p").style.display = "block";
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("close").style.display = "block";
+}
+
+function mobileClose(){
+    document.getElementById("m-p").style.display = "none";
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("close").style.display = "none";
+}
