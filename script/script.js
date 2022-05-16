41f512f595a3cf9b0eddefa4fbe956745322d85f
zecawuw <88804350+zecawuw@users.noreply.github.com>
2022-05-16 15:27:53 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -10,27 +10,39 @@ function mobileClose(){
     document.getElementById("close").style.display = "none";
 }
 
-var urls = [
-    'https://viewplayer.online/embed/thebigbangtheory/',
-    'https://viewplayer.online/embed/legacies/',
-    'https://viewplayer.online/embed/moon-knight/',
-    'https://viewplayer.online/embed/peakyblinders/',
-];
+// var i = 0;
+// var urls = [
+//     'https://viewplayer.online/embed/thebigbangtheory/',
+//     'https://viewplayer.online/embed/legacies/',
+//     'https://viewplayer.online/embed/moon-knight/',
+//     'https://viewplayer.online/embed/peakyblinders/',
+// ];
 
-for(let index = 0; index <= urls.length; index++) {
-let iframe = document.getElementById('idIframe');
-iframe.src = urls[index]
-}
+// var iframe = document.getElementById('idi').src = urls[i];
+
+// for(let i = 0; i <= urls.length; i++) {
+// let iframe = document.getElementById('idIframe');
+// iframe.src = urls[i]
+// }
+
+    // document.getElementById("idIframe").style.display = "block";
+
+// function Iframe() {
+//     var elem = document.getElementById('idIframe');
+//     if(typeof elem !== 'undefined' && elem !== null) {
+//       console.log('ola mundo')
+//     }
+//   }
 
 
-// var iframe = document.getElementById('thebigbangtheory');
-// iframe1.src = urls[0];
-
-// var iframe = document.getElementById('legacies');
-// iframe2.src = urls[1];
-
-// var iframe = document.getElementById('MoonKnight');
-// iframe3.src = urls[2];
-
-// var iframe = document.getElementById('peakyblinders');
-// iframe4.src = urls[3];
+//   function Iframe(){
+//         document.getElementById("destaque").style.display = 'none';
+//         document.getElementById("idi").style.display = 'inline-block';
+//         console.log('ola mundo');
+//     }
+    
+    // var elem = document.getElementById("idi");
+    // if(typeof elem !== null && elem !== 'undefined' ) {
+    //   document.getElementById("idi").innerHTML = "display = 'block'"
+    // }
+  
